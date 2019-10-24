7b608fc5a6eef763044975e6c2c8b6dbd23c4c9c
Erik Miller <eamiller34@gmail.com>
2019-10-24 14:58:01 -0700

    Initial Commit

---
+++ learn/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  learn
+//
+//  Created by Erik Miller on 10/24/19.
+//  Copyright © 2019 Erik Miller. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
